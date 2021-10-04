ae9e42c67fe2cd7f4d32d547f422dd8564e36b78
Marko <marko.cuk.888@gmail.com>
2021-10-04 19:20:52 +0200

    adding SplashScreen and hide navbar when in app

--- app/src/main/java/com/example/cartoonclassification/MainActivity.java
+++ app/src/main/java/com/example/cartoonclassification/MainActivity.java
@@ -85,6 +85,14 @@ public class MainActivity extends AppCompatActivity {
         imageView=(ImageView)findViewById(R.id.image);
         buclassify=(Button)findViewById(R.id.classify);
         prediction=(TextView)findViewById(R.id.predictions);
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
 btnCam = (Button) findViewById(R.id.btnCam);
 btnCam.setOnClickListener(new View.OnClickListener() {
